89c2aaf5bb2a283f85c0770b62253bd53a882ab1
GenaroGandolfo <ggandolfobrignoli@udesa.edu.ar>
2021-04-26 16:05:57 -0300

    fin

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const homeController = require('../controllers/homeController');
 
 /* GET home page. */
-router.get('/', homeController.index);
+router.get('/index', homeController.index);
 router.get('/product/:id', homeController.product);
 
 module.exports = router;
